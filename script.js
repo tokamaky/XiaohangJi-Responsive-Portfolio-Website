d5dc960cbe687cb3b4d30f894e4c3b520d34b4d2
tokamaky <jxh186045@gmail.com>
2023-08-16 11:42:33 -0400

    replace new resume and footer sticky

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".menu-icon");
     menu.classList.toggle("open");
-    icon.classList.toggle("open");
-    
+    icon.classList.toggle("open"); 
 }
