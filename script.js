fe3ec4b4df23c93fc7134ade813b6e6a09df033c
Xiaohang Ji <jxh186045@gmail.com>
2023-09-17 21:59:49 -0400

    debug

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ function toggleMenu() {
 // JavaScript function to change the project
 let currentProject = 1; // Start with the first project
 
-function changeProject(direction) {
-  currentProject += direction;
+function changeProject(direction,toArrows) {
+  if(toArrows){
+    currentProject = toArrows
+  }else{
+    currentProject += direction;
+  }
 
   // Limit the project number to 1, 2, or 3 (assuming you have 3 projects)
   if (currentProject < 1) {
@@ -19,40 +23,86 @@ function changeProject(direction) {
   }
 
   // Update the project content based on the currentProject value
-  // You can fetch data dynamically or use static data as per your requirements
-  // For simplicity, I'm assuming static data here
   const projectTitle = document.querySelector('.project-title');
   //const projectDescription = document.querySelector('.project-description');
-  const projectImg = document.querySelector('.project-img');
+  const projectImg = document.querySelector('.project-img')
+  const whiteArrows = document.querySelectorAll('.white-arrows span')
+  const projectText = document.querySelector('.project-description')
+  
 
   switch (currentProject) {
     case 1:
       projectTitle.textContent = 'Project One';
       //projectDescription.textContent = 'This is a description of Project One.';
       projectImg.src = './assets/project-1.png';
+      projectText.innerHTML='asdasdasdasd';
+      whiteArrows[0].style.backgroundColor = 'red'
+      whiteArrows[1].style.backgroundColor = 'white'
+      whiteArrows[2].style.backgroundColor = 'white'
       break;
     case 2:
       projectTitle.textContent = 'Project Two';
      // projectDescription.textContent = 'This is a description of Project Two.';
       projectImg.src = './assets/project-2.png';
+      projectText.innerHTML='1234151235';
+      whiteArrows[0].style.backgroundColor = 'white'
+      whiteArrows[1].style.backgroundColor = 'red'
+      whiteArrows[2].style.backgroundColor = 'white'
       break;
     case 3:
       projectTitle.textContent = 'Project Three';
       //projectDescription.textContent = 'This is a description of Project Three.';
       projectImg.src = './assets/project-3.png';
+      projectText.innerHTML='123415123234523423dgfsdfsd5';
+      whiteArrows[0].style.backgroundColor = 'white'
+      whiteArrows[1].style.backgroundColor = 'white'
+      whiteArrows[2].style.backgroundColor = 'red'
       break;
   }
 }
 
-// Function to open GitHub link
-function openGitHub(url) {
-  window.open(url, '_blank');
-}
 
-// Function to open Live Demo link
-function openDemo(url) {
-  window.open(url, '_blank');
-}
+let projectBtn1 = document.querySelector('.project-btn1')
+let projectBtn2 = document.querySelector('.project-btn2')
+projectBtn1.addEventListener('click',()=>{
+  if(currentProject==1){
+    window.open('https://www.bilibili.com/')
+  }else if(currentProject==2){
+    window.open()
+  }else{
+    window.open()
+  }
+})
+projectBtn2.addEventListener('click',()=>{
+  if(currentProject==1){
+    window.open()
+  }else if(currentProject==2){
+    window.open('https://www.bilibili.com/')
+  }else{
+    window.open()
+  }
+})
 
 // Initialize the project content
-changeProject(0);
+changeProject(0,null);
+
+
+const projects = document.querySelector('#projects')
+projects.addEventListener('mouseenter',()=>{
+  stop()
+})
+projects.addEventListener('mouseleave',()=>{
+  start()
+})
+
+// 开始定时器
+function start() {
+  timer = setInterval(function () {
+    changeProject(1,null)
+  }, 2000);
+}
+start();
+// 关闭定时器
+function stop() {
+  clearInterval(timer);
+}
